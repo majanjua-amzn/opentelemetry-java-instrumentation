573c598cc5ff3fd847a33f1563c62b14cbe6bed0
Mahad Janjua <majanjua@amazon.com>
2025-08-15 14:30:00 -0700

    .

--- benchmark-overhead/src/test/java/io/opentelemetry/config/Configs.java
+++ benchmark-overhead/src/test/java/io/opentelemetry/config/Configs.java
@@ -23,7 +23,7 @@ public enum Configs {
               Agent.NONE)
           .warmupSeconds(60)
           .maxRequestRate(100)
-          .totalIterations(500) // 50 = 10 seconds
+          .totalIterations(5000) // 50 = 10 seconds
           .build()),
   TPS_1000(
       TestConfig.builder()
@@ -37,7 +37,7 @@ public enum Configs {
               Agent.NONE)
           .warmupSeconds(60)
           .maxRequestRate(1000)
-          .totalIterations(5000) // 50 = 2 seconds
+          .totalIterations(25000) // 50 = 2 seconds
           .build());
 
   public final TestConfig config;
